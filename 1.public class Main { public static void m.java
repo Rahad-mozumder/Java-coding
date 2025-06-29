b19f73b57369557328170b3497bbffb5b15d79b6
Rahad-mozumder <rahadul39@gmail.com>
2025-06-30 02:44:18 +0600

    text code added by me

---
+++ 1.public class Main { public static void m.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Rahadul islam";
+        String studentId = "E243022";
+        String age = "23";
+        String email = "rahadul39@gmail.com";
+        String phone = "01865461459";
+        String address = "Feni";
+        
+        System.out.println("Name: " + name);
+        System.out.println("StudentId: " + studentId);
+        System.out.println("Age: " + age);
+        System.out.println("Email: " + email);
+        System.out.println("Phone: " + phone);
+        System.out.println("Address: " + address);
+    }
+}
